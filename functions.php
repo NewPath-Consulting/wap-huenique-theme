cab15f35e59af26644dbc7a59abb8eee3a4313f4
Natalie Brotherton <brona@umich.edu>
2022-10-25 11:55:26 -0400

    theme check suggestions

--- functions.php
+++ functions.php
@@ -12,4 +12,10 @@ function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
+add_action('after_setup_theme', 'wap_add_theme_support');
+function wap_add_theme_support() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'automatic-feed-links' ); 
+}
+
 $customizer = new Generatepress_Child_Customizer();
